6073f10b6754dec2237bf4f04ade50a09af1ec85
Bradley Balster <bbalster87@gmail.com>
2022-08-01 20:42:34 +0300

    Created HTML boilerplate and initial javascript pseudo code

---
+++ sketch.js
@@ -0,0 +1,23 @@
+// This script allows the user to play with an Etch-A-Sketch
+
+function createDrawingBoard () {
+  // Creates a grid of divs that is N by N to be used as the drawing board
+  // Returns the default starting position for the drawing cursor
+}
+
+function colorSquare () {
+  // Colors the provided square black
+}
+
+function moveCursor () {
+  // Moves the drawing cursor in a cardinal direction to color a square
+  // Returns the new position of the drawing cursor
+}
+
+function resetDrawing () {
+  // Resets colors on all squares except currently selected
+}
+
+function setDrawingMode () {
+  // Toggle between mouse and keyboard drawing modes
+}
