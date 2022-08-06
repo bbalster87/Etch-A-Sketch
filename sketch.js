126ba95f204cd061781290f410e79c087ad9c237
Bradley Balster <bbalster87@gmail.com>
2022-08-06 20:36:57 +0300

    Completed createDrawingBoard function Added some styling to make it easier to see

--- sketch.js
+++ sketch.js
@@ -1,8 +1,19 @@
 // This script allows the user to play with an Etch-A-Sketch
 
-function createDrawingBoard () {
+function createDrawingBoard (x, y) {
   // Creates a grid of divs that is N by N to be used as the drawing board
   // Returns the default starting position for the drawing cursor
+  container = document.getElementById("container");
+  for (let i = 0; i <= x; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j <= y; j++) {
+      cell = document.createElement("div");
+      cell.className = "cell";
+      row.appendChild(cell);
+    }
+    container.appendChild(row)
+  }
 }
 
 function colorSquare () {
@@ -21,3 +32,5 @@ function resetDrawing () {
 function setDrawingMode () {
   // Toggle between mouse and keyboard drawing modes
 }
+
+createDrawingBoard(20, 20);
